62c4e3f8fd2ab762af0f655f2b8e39c4dc8c69d7
Ian Miranda <ian_secchin@hotmail.com>
2018-10-23 08:48:27 -0300

    Creates main object for the system

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,20 @@
+package tweeconomics
+
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream._
+import org.apache.spark.streaming.twitter._
+
+object App {
+
+    Logger.setup()
+
+    val config = new SparkConf().setAppName("tweeconomics").setMaster("local[*]")
+    val sc = new SparkContext(config)
+
+    def main(args: Array[String]) {
+        val streaming = new Streaming()
+        streaming.setupAnalyzer()
+        streaming.start()
+    }
+}
