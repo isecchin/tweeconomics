555cbbc4d63c944ea38d6a84a565a5a76a883778
Ian Miranda <ian_secchin@hotmail.com>
2018-11-08 16:07:07 -0200

    Imports SQL context to main app

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 package tweeconomics
 
 import org.apache.spark._
+import org.apache.spark.sql._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.dstream._
 import org.apache.spark.streaming.twitter._
@@ -11,6 +12,8 @@ object App
 
     val config = new SparkConf().setAppName("tweeconomics").setMaster("local[*]")
     val sc = new SparkContext(config)
+    val sqlContext = new SQLContext(sc)
+    import sqlContext.implicits._
 
     def main(args: Array[String]) = {
         val streaming = new Streaming()
