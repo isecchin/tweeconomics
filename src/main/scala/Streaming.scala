bda49d9ce43f918c8bae1be892a51cfe623f98b2
Tamine Verli <tamine.verli@gmail.com>
2018-11-20 23:39:47 -0200

    Creates case class for Twitter Data

--- src/main/scala/Streaming.scala
+++ src/main/scala/Streaming.scala
@@ -8,7 +8,7 @@ import org.apache.spark.streaming.twitter._
 import java.time.LocalDateTime
 import scala.collection._
 
-case class TweetData(company: String, date: String, sentiment: Int)
+case class TweetData(company_id: Long, sentiment_id: Int, date: String)
 
 class Streaming
 {
